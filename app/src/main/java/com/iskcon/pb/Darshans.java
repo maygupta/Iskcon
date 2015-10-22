c44526adb82414a42f59085ee7cdbd267e59b6d7
Mayank Gupta <maygupta@groupon.com>
2015-10-21 23:12:50 -0700

    Cleaned darshans tab rendering using Picasso

--- app/src/main/java/com/iskcon/pb/Darshans.java
+++ app/src/main/java/com/iskcon/pb/Darshans.java
@@ -2,160 +2,41 @@ package com.iskcon.pb;
 
 import android.animation.Animator;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
+import java.util.ArrayList;
 
 public class Darshans  extends Activity
 {
-
-    private Animator mCurrentAnimator;
-    private int mShortAnimationDuration;
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.darshans);
-        GridView gridview = (GridView) findViewById(R.id.gridview);
+        ListView gridview = (ListView) findViewById(R.id.gridview);
 
         Intent intent = getIntent();
         String jsonStr = intent.getStringExtra("darshans");
 
         try {
-            gridview.setAdapter(new ImageAdapter(this, jsonStr));
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-    public class ImageAdapter extends BaseAdapter {
-        private Context mContext;
-        private String mJson;
-        public int getCount() {
-            return mThumbIds.length;
-        }
-        public Object getItem(int position) {
-            return mThumbIds[position];
-        }
-        public long getItemId(int position) {
-            return 0;
-        }
-        private String[] mThumbIds;
-        private String[] mDescriptions;
-
-        public ImageAdapter(Context c, String json) throws JSONException {
-            mContext = c;
-            mJson = json;
-            JSONObject darshans = new JSONObject(json);
+            JSONObject darshans = new JSONObject(jsonStr);
             JSONArray darshansArray = darshans.getJSONArray("darshans");
-
-            mThumbIds = new String[darshansArray.length()];
-            mDescriptions = new String[darshansArray.length()];
+            ArrayList<DarshanImage>images = new ArrayList<>();
 
             for(int i = 0; i < darshansArray.length(); i++) {
                 JSONObject row = darshansArray.getJSONObject(i);
-                mThumbIds[i] = row.getString("url");
-                mDescriptions[i] = row.getString("name");
+                DarshanImage image = new DarshanImage(row.getString("url"), row.getString("name"));
+                images.add(image);
             }
-
-        }
-
-        public View getView(int position, View convertView, ViewGroup parent) {
-            final ImageView imageView;
-            if (convertView == null){
-                imageView = new ImageView(mContext);
-                imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
-                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-                imageView.setPadding(8, 8, 8, 8);
-            }
-            else{
-                imageView = (ImageView) convertView;
-            }
-
-            int loader = R.drawable.krishna_balram;
-
-            final String imageUrl = mThumbIds[position];
-
-            ImageLoader imageLoader = new ImageLoader(getApplicationContext());
-
-            imageLoader.DisplayImage(imageUrl, 0, imageView);
-            imageView.getLayoutParams().height = 300;
-            imageView.getLayoutParams().width = 300;
-
-            final String imageDescription = mDescriptions[position];
-
-            imageView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    try {
-                        zoomImageFromThumb(imageView, imageUrl, imageDescription);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-            });
-            return imageView;
-        }
-
-
-        private void zoomImageFromThumb(final View thumbView, String imageUrl, String description) throws IOException {
-            // If there's an animation in progress, cancel it
-            // immediately and proceed with this one.
-            if (mCurrentAnimator != null) {
-                mCurrentAnimator.cancel();
-            }
-
-            // Load the high-resolution "zoomed-in" image.
-            final ImageView expandedImageView = (ImageView) findViewById(
-                    R.id.expanded_image);
-
-            ImageLoader imageLoader = new ImageLoader(getApplicationContext());
-
-            imageLoader.DisplayImage(imageUrl, 0, expandedImageView);
-
-            final Button backButton = (Button) findViewById(R.id.back_button);
-            final TextView textView = (TextView) findViewById(R.id.image_description);
-            textView.setText(description);
-
-            expandedImageView.setVisibility(View.VISIBLE);
-            final GridView gridView = (GridView) findViewById(R.id.gridview);
-            gridView.setVisibility(View.INVISIBLE);
-            backButton.setVisibility(View.VISIBLE);
-            textView.setVisibility(View.VISIBLE);
-
-            backButton.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View view) {
-                    expandedImageView.setVisibility(View.GONE);
-                    gridView.setVisibility(View.VISIBLE);
-                    backButton.setVisibility(View.GONE);
-                    textView.setVisibility(View.GONE);
-                }
-            });
-
-            expandedImageView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    expandedImageView.setVisibility(View.GONE);
-                    gridView.setVisibility(View.VISIBLE);
-                    backButton.setVisibility(View.GONE);
-                    textView.setVisibility(View.GONE);
-                }
-            });
+            gridview.setAdapter(new ImageAdapter(this, images));
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
     }
 }
---
+++ app/src/main/java/com/iskcon/pb/ImageAdapter.java
@@ -0,0 +1,43 @@
+package com.iskcon.pb;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * Created by maygupta on 10/21/15.
+ */
+public class ImageAdapter extends ArrayAdapter<DarshanImage> {
+        private Context mContext;
+
+        public ImageAdapter(Context c, List<DarshanImage> images) {
+            super(c,0,images);
+            mContext = c;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            DarshanImage image = getItem(position);
+
+            if (convertView == null){
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.image_layout, parent, false);
+            }
+
+            ImageView ivImage = (ImageView) convertView.findViewById(R.id.ivDarshan);
+            //TextView tvDescription = (TextView) convertView.findViewById(R.id.tvDarshanDescription);
+
+            //tvDescription.setText(image.description);
+
+            ivImage.setImageResource(0);
+            Picasso.with(mContext).load(image.url).into(ivImage);
+
+            return ivImage;
+        }
+
+    }
